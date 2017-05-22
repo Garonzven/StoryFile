47762b1a0db24a5759cb5fb5397ca40846f4b9ef
DanielSoto <daniel30sotom@gmail.com>
2017-05-22 10:51:21 -0400

    first

---
+++ Story_File/Assets/Scripts/Test.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour {
+
+	public bool test;
+	[SerializeField]
+	private bool otherTest;
+
+	#if MY_SYMBOL
+	void Awake()
+	{
+		
+	}
+	#endif
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void OnEnable(){
+		//GameObject.find
+	}
+}
