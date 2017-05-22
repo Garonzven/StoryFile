9c53fe38e6da31dc390f59a71ee676490a7494e0
DanielSoto <daniel30sotom@gmail.com>
2017-05-22 13:13:17 -0400

    fixed microphone class issues

---
+++ Story_File/Assets/Scripts/MicrophoneListener.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Utils
+{
+	[RequireComponent( typeof( AudioSource ) )]
+	public class MicrophoneListener : MonoBehaviour {
+
+		// Use this for initialization
+		void Start () {
+			AudioSource aud = GetComponent<AudioSource>();
+			aud.clip = Microphone.Start("Built-in Microphone", true, 10, 44100);
+			aud.Play();
+		}	
+	}
+}
