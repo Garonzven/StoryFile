67913617761faf6f22baeeaf834b342d80df5f1f
DanielSoto <daniel30sotom@gmail.com>
2017-05-22 13:10:23 -0400

    microphone

---
+++ Story_File/Assets/Scripts/Microphone.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent( typeof( AudioSource ) )]
+public class Microphone : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		AudioSource aud = GetComponent<AudioSource>();
+		aud.clip = Microphone.Start("Built-in Microphone", true, 10, 44100);
+		aud.Play();
+	}	
+}
