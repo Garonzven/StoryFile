5ed725e8bfbee4cbcf26bb737a33ba25fb98a471
DanielSoto <daniel30sotom@gmail.com>
2017-05-24 11:17:02 -0400

    server connection still in progress

--- Story_File/Assets/Scripts/RequestHandler.cs
+++ Story_File/Assets/Scripts/RequestHandler.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using UnityEngine.Networking;
 using WebSocketSharp;
 using DDK.Base.Extensions;
+using System;
 
 namespace StoryFile
 {
@@ -13,31 +14,36 @@ namespace StoryFile
 		public AudioSource audioSource;
 
 
+		WebSocket _ws;
+		byte[] _clip;
+		//string _clip;
+
+
 		/// <summary>
 		/// The Transcription's web socket uri
 		/// </summary>
-		private const string WSS_TRANSCRIPTION_URI = "wss://storyfilestage.com:7070";
+		private const string WSS_TRANSCRIPTION_URI = "wss://storyfilestage.com:7070"; //ws://echo.websocket.org/
 
 
 		// Use this for initialization
 		void Start () {
-			using (var ws = new WebSocket ( WSS_TRANSCRIPTION_URI /*"ws://dragonsnest.far/Laputa"*/ )) {
-				ws.OnMessage += (sender, e) => Debug.Log ("Laputa says: " + e.Data);
-				ws.OnOpen += (sender, e) => { 
-					Debug.Log ("Connected");
-					Debug.Log (ws.Ping ());
-				};
-				ws.OnError += (sender, e) => Debug.LogError ("Laputa says: " + e.Message);
-				ws.OnClose += (sender, e) => Debug.Log ("Connection closed");
+			_clip = audioSource.clip.EncodeToWav ();
+			//_clip = Convert.ToBase64String( audioSource.clip.EncodeToWav () );
+			//_ws = new WebSocket (WSS_TRANSCRIPTION_URI);
+			_ws = new WebSocket ("ws://echo.websocket.org/");
+			//_ws.SetCredentials ("foo@bar.com", "opensource", true);
+			_ws.EmitOnPing = true;
 
-				ws.EmitOnPing = true;
+			_ws.OnMessage += OnMessage;
+			_ws.OnOpen += OnOpen;
+			_ws.OnError += OnError;
+			_ws.OnClose += OnClose;
 
-				ws.ConnectAsync ();
-				ws.Send ( audioSource.clip.EncodeToWav () );
-			}
+			_ws.ConnectAsync ();
 
 			//SendRequest ();
 		}
+
 		public void SendRequest()
 		{
 			StartCoroutine ( SendRequestAndWait () );
@@ -46,10 +52,19 @@ namespace StoryFile
 
 		IEnumerator SendRequestAndWait()
 		{
-			DownloadHandlerBuffer downloadHandler = new DownloadHandlerBuffer ();
-			UnityWebRequest request = new UnityWebRequest (url, "GET");
-			request.downloadHandler = downloadHandler;
-			Debug.Log ("Starting Download..");
+			//DownloadHandlerBuffer downloadHandler = new DownloadHandlerBuffer ();
+			Dictionary<string, object> bodyData = new Dictionary<string, object> () {
+				{ "email", "foo@bar.com" },
+				{ "password", "opensource" }
+			};
+			UploadHandler uploadHandler = new UploadHandlerRaw ( Convert.FromBase64String ( bodyData.Serialize () ) ) {
+				contentType = "application-json",
+			};
+			UnityWebRequest request = new UnityWebRequest (url, "POST");
+			request.uploadHandler = uploadHandler;
+			request.SetRequestHeader ("APP-TOKEN", "secureapptoken");
+			request.SetRequestHeader ("Content-Length", "60");	
+
 			AsyncOperation asyncOperation = request.Send ();
 			yield return asyncOperation;
 			Debug.Log (asyncOperation.isDone);
@@ -61,8 +76,32 @@ namespace StoryFile
 			else
 			{
 				Debug.Log (request.downloadHandler.data);
-
 			}
 		}
+		#region Callbacks
+		void OnMessage( object sender, MessageEventArgs e )
+		{
+			Debug.Log ("website says: " + e.Data);
+		}
+		void OnOpen( object sender, EventArgs e )
+		{ 
+			Debug.Log ("Connected");
+			Debug.Log (_ws.Ping ());
+			_ws.SendAsync ("Balus", OnSent);
+			//_ws.SendAsync ( _clip, OnSent );
+		}
+		void OnClose( object sender, CloseEventArgs e )
+		{
+			Debug.Log ("Connection closed");
+		}
+		void OnError( object sender, ErrorEventArgs e )
+		{
+			Debug.LogError ("website says: " + e.Message);
+		}
+		void OnSent( bool completed )
+		{
+			Debug.Log ( "Sent: " + completed );
+		}
+		#endregion
 	}
 }
