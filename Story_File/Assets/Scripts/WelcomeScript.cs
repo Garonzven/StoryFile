c6c5b6564b6923f8365919622a62fd23ce1e64aa
jcgb15 <jeanma.jcgb15@gmail.com>
2017-05-24 10:22:25 -0400

    panels

---
+++ Story_File/Assets/Scripts/WelcomeScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Utils
+{
+    public class WelcomeScript : MonoBehaviour
+    {
+        public GameObject welcomePanel;
+        public GameObject amandaPanel;
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        public void GetStarted()
+        {
+            welcomePanel.gameObject.SetActive(false);
+            amandaPanel.gameObject.SetActive(true);
+        }
+
+        // Update is called once per frame
+        /*void Update () {
+		
+        }*/
+    }
+}
+
