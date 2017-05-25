5977bb2cb386f4286426dd372a5f33c379844ad1
jcgb15 <jeanma.jcgb15@gmail.com>
2017-05-25 10:59:15 -0400

    eliminado archivos innecesarios

---
+++ Story_File/Assets/Scripts/AmandaHandler.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class AmandaHandler : MonoBehaviour
+{
+    public Text txtTalk;
+    private const string HOLD_TEXT = "HOLD TO TALK";
+    private const string RELEASE_TEXT = "RELEASE TO LISTEN";
+
+
+    public void onPointerDownButton()
+    {
+        txtTalk.text = "RELEASE TO LISTEN";
+    }
+    public void onPointerUpButton()
+    {
+        txtTalk.text = "HOLD TO TALK";
+    }
+}
