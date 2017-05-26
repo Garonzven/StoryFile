8a40dfa1c81b40cd52c5fbdf53822dd24bf5f3af
jcgb15 <jeanma.jcgb15@gmail.com>
2017-05-26 11:22:11 -0400

    watson speech to text handler examples

---
+++ Story_File/Assets/Scripts/AmandaSpeechToText.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using IBM.Watson.DeveloperCloud.Services.SpeechToText.v1;
+using IBM.Watson.DeveloperCloud.Logging;
+
+public class WatsonSpeechToText : MonoBehaviour
+{
+    [SerializeField]
+    private AudioClip m_AudioClip = new AudioClip();
+    private SpeechToText m_SpeechToText = new SpeechToText();
+
+    void Start()
+    {
+        m_SpeechToText.Recognize(m_AudioClip, HandleOnRecognize);
+    }
+
+    void HandleOnRecognize(SpeechRecognitionEvent result)
+    {
+        if (result != null && result.results.Length > 0)
+        {
+            foreach (var res in result.results)
+            {
+                foreach (var alt in res.alternatives)
+                {
+                    string text = alt.transcript;
+                    Debug.Log(string.Format("{0} ({1}, {2:0.00})\n", text, res.final ? "Final" : "Interim", alt.confidence));
+                }
+            }
+        }
+    }
+
+}
+
